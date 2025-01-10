b0d4ef13afeea14a20cb0048b9bb5535950d9afd
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-01-10 16:59:54 +0300

    v1.1

---
+++ sally/test-here/testHere.js
@@ -0,0 +1,45 @@
+
+
+function go_to(testId) {
+    // تخزين testId في الـ sessionStorage
+    sessionStorage.setItem('testId', testId);
+
+    
+    // الانتقال إلى صفحة الاختبار
+    window.location.href = '../tests/test.html';
+}
+
+window.onload = () => {
+    if (sessionStorage.getItem('iq') == 'true' && sessionStorage.getItem('tech') == 'true' && sessionStorage.getItem('eng') == 'true') {
+        document.getElementById('s3').disabled = true;
+        document.getElementById('r3').disabled = false;
+        document.getElementById('s2').disabled = true;
+        document.getElementById('r2').disabled = false;
+        document.getElementById('s1').disabled = true;
+        document.getElementById('r1').disabled = false;
+    } else if (sessionStorage.getItem('tech') == 'true' && sessionStorage.getItem('eng') == 'true') {
+        document.getElementById('s3').disabled = true;
+        document.getElementById('r3').disabled = false;
+        document.getElementById('s2').disabled = true;
+        document.getElementById('r2').disabled = false;
+    } else if (sessionStorage.getItem('iq') == 'true' && sessionStorage.getItem('eng') == 'true') {
+        document.getElementById('s1').disabled = true;
+        document.getElementById('r1').disabled = false;
+        document.getElementById('s2').disabled = true;
+        document.getElementById('r2').disabled = false;
+    } else if (sessionStorage.getItem('iq') == 'true' && sessionStorage.getItem('tech') == 'true') {
+        document.getElementById('s3').disabled = true;
+        document.getElementById('r3').disabled = false;
+        document.getElementById('s1').disabled = true;
+        document.getElementById('r1').disabled = false;
+    } else if (sessionStorage.getItem('iq') == 'true') {
+        document.getElementById('s1').disabled = true;
+        document.getElementById('r1').disabled = false;
+    } else if (sessionStorage.getItem('eng') == 'true') {
+        document.getElementById('s2').disabled = true;
+        document.getElementById('r2').disabled = false;
+    } else if (sessionStorage.getItem('tech') == 'true') {
+        document.getElementById('s3').disabled = true;
+        document.getElementById('r3').disabled = false;
+    }
+};
